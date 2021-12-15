33540c9247a9fc1cc6ffbb5da287136404be9130
Mariusz Klochowicz <mariusz@klochowicz.com>
2021-12-16 09:55:23 +1030

    Remove workaround for longer heartbeat interval in debug mode
    Before contract setup was split into dedicated actors on each side, heartbeats
    were not being delivered correctly and the taker would close the connection
    half-way through contract setup.
    This was especially noticeable in debug mode, as the contract setup could take even
    ~60 seconds to complete. A workaround was set in place (45s for heartbeat
    interval, making it drop connection only after 90s, longer than contract setup).

--- daemon/src/lib.rs
+++ daemon/src/lib.rs
@@ -68,12 +68,9 @@ pub mod tx;
 pub mod wallet;
 pub mod wire;
 
-// Certain operations (e.g. contract setup) take long time in debug mode,
-// causing us to lag behind in processing heartbeats.
-// Increasing the value for debug mode makes sure that we don't cause problems
-// when testing / CI, whilst the release can still detect status faster
-pub const HEARTBEAT_INTERVAL: std::time::Duration =
-    Duration::from_secs(if cfg!(debug_assertions) { 45 } else { 5 });
+/// Duration between the heartbeats sent by the maker, used by the taker to
+/// determine whether the maker is online.
+pub const HEARTBEAT_INTERVAL: std::time::Duration = Duration::from_secs(5);
 
 pub const N_PAYOUTS: usize = 200;
 
